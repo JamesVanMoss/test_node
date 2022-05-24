86cdc2a41651b45ddd9df8b0a53eb7c036aa42a2
Valax-!i <82119964+JamesVanMoss@users.noreply.github.com>
2022-05-24 08:58:31 -0500

    Update git_updater.py

--- git_updater.py
+++ git_updater.py
@@ -2,9 +2,9 @@ import subprocess
 import time
 import os
 
-# os.system("npm start")
-cmd = "npm start"
-subprocess.Popen(cmd, shell=True)
+os.system("git pull origin main")
+
+subprocess.Popen("npm start", shell=True)
 
 while 1 == 1:
 	os.system("git pull origin main")
