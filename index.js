9b1853d6d82a54eb3e0a43d154dfdd6e044e3158
Valax-!i <82119964+JamesVanMoss@users.noreply.github.com>
2022-06-01 16:53:16 -0500

    it works

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const c = 9
+const http = require ('http'). createServer();
 
-console.log(c + 1)
+const io = require ('socket.io')(http,{
+    cors: {origin: '*'}
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', message);
+    });
+});
+
+const port = 8080;
+http.listen(port, () => console.log('listening on port '+port));
